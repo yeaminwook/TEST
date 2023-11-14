9251276259d653409e4bcc4358ff82dfd82497bc
yeaminwook <ymw@m3sen.com>
2023-11-14 13:40:21 +0900

    수정Update Hello.c

--- Project1/Hello.c
+++ Project1/Hello.c
@@ -6,10 +6,19 @@ int main() {
 	printf("hello.\n");
 	printf("hello.\n");
 	printf("hello.\n");
-	printf("hello.\n");
-	printf("hello.\n");
-	printf("hello.\n");
-	printf("hello.\n");
+
+	printf("git test.\n"); 
+	printf("git test.\n");
+	printf("git test.\n");
+
+	printf("git change commit.\n");
+	printf("git change commit.\n");
+	printf("git change commit.\n");
+	
+	
+	
+
+
 	
 
 	return 0;
