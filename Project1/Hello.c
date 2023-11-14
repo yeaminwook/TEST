42070ec4f6ee9ff9ae42e9a2335531d4e520cf45
yeaminwook <150656520+yeaminwook@users.noreply.github.com>
2023-11-14 11:44:55 +0900

    Update Hello.c

--- Project1/Hello.c
+++ Project1/Hello.c
@@ -4,6 +4,13 @@
 int main() {
 
 	printf("hello.\n");
+	printf("hello.\n");
+	printf("hello.\n");
+	printf("hello.\n");
+	printf("hello.\n");
+	printf("hello.\n");
+	printf("hello.\n");
+	
 
 	return 0;
 }
